5dd32541ea6fcda242d4bdaa27e1825c4a9f636f
Vitaliy-colab <vitaliyzaritskuy@gmail.com>
2023-11-25 19:35:57 +0200

    no message

--- Завдання 1.py
+++ Завдання 1.py
@@ -1,8 +1,8 @@
 import math
 def t(u,v):
-    t=(u-math.log(v))/(math.log(u)**2)
+    t=(u-math.log(v))/(math.log(u)**2) #Функція для розвязку
     return t
 a=int(input("Введіть а: "))
 x=int(input("Введіть x: "))
-z=t(x,a)*math.sin(t(x,a))
+z=t(x,a)*math.sin(t(x,a)) #Основна функція
 print("z= %3.4f" %z)
